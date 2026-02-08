2bcb030d8f6eb0bc21c1a547deff45f33839be8d
Omar Sameh <omar.sameh.m.i@gmail.com>
2026-02-08 18:19:37 +0200

    making kml more visible and observable

--- lib/data/kml_maker.dart
+++ lib/data/kml_maker.dart
@@ -8,13 +8,14 @@ class KMLMaker {
             <name>Logo</name>
             <ScreenOverlay>
                 <name>Logo</name>
+                <description>LG Master Logo</description>
                 <Icon>
                     <href>$imageUrl</href>
                 </Icon>
-                <overlayXY x="0" y="1" xunits="fraction" yunits="fraction"/>
-                <screenXY x="0.02" y="0.95" xunits="fraction" yunits="fraction"/>
-                <rotationXY x="0" y="0" xunits="fraction" yunits="fraction"/>
-                <size x="500" y="400" xunits="pixels" yunits="pixels"/>
+                <overlayXY x="0.5" y="0.5" xunits="fraction" yunits="fraction"/>
+                <screenXY x="0.5" y="0.5" xunits="fraction" yunits="fraction"/>
+                <rotationXY x="0.5" y="0.5" xunits="fraction" yunits="fraction"/>
+                <size x="800" y="600" xunits="pixels" yunits="pixels"/>
             </ScreenOverlay>
         </Folder>
     </Document>
@@ -32,43 +33,135 @@ class KMLMaker {
   }
 
   static String generatePyramid() {
-    // colored pyramid above cairo
+    // 3D pyramid with vibrant colors above cairo
     return '''<?xml version="1.0" encoding="UTF-8"?>
 <kml xmlns="http://www.opengis.net/kml/2.2">
 <Document>
   <name>GSoC Pyramid</name>
-  <Style id="yellowPoly">
-    <LineStyle><color>7f7faaaa</color><width>4</width></LineStyle>
-    <PolyStyle><color>7f00ffff</color></PolyStyle>
+  
+  <Style id="pyramidBase">
+    <LineStyle><color>ff0000ff</color><width>2</width></LineStyle>
+    <PolyStyle><color>dd00ff00</color></PolyStyle>
   </Style>
+  
+  <Style id="pyramidFace1">
+    <LineStyle><color>ffff0000</color><width>2</width></LineStyle>
+    <PolyStyle><color>ddff0000</color></PolyStyle>
+  </Style>
+  
+  <Style id="pyramidFace2">
+    <LineStyle><color>ff00ff00</color><width>2</width></LineStyle>
+    <PolyStyle><color>dd00ff00</color></PolyStyle>
+  </Style>
+  
+  <Style id="pyramidFace3">
+    <LineStyle><color>ffffff00</color><width>2</width></LineStyle>
+    <PolyStyle><color>ddffff00</color></PolyStyle>
+  </Style>
+  
+  <Style id="pyramidFace4">
+    <LineStyle><color>ffff00ff</color><width>2</width></LineStyle>
+    <PolyStyle><color>ddff00ff</color></PolyStyle>
+  </Style>
+
+  <!-- Base -->
   <Placemark>
-    <name>Cairo Pyramid</name>
-    <styleUrl>#yellowPoly</styleUrl>
+    <name>Pyramid Base</name>
+    <styleUrl>#pyramidBase</styleUrl>
     <Polygon>
-      <extrude>1</extrude>
+      <extrude>0</extrude>
       <altitudeMode>relativeToGround</altitudeMode>
       <outerBoundaryIs>
         <LinearRing>
           <coordinates>
-            31.235,30.044,0
-            31.238,30.044,0
-            31.238,30.047,0
-            31.235,30.047,0
-            31.235,30.044,0
+            31.233,30.042,0
+            31.239,30.042,0
+            31.239,30.048,0
+            31.233,30.048,0
+            31.233,30.042,0
           </coordinates>
         </LinearRing>
       </outerBoundaryIs>
-      <innerBoundaryIs>
+    </Polygon>
+  </Placemark>
+
+  <!-- Face 1 (North) -->
+  <Placemark>
+    <name>Pyramid Face North</name>
+    <styleUrl>#pyramidFace1</styleUrl>
+    <Polygon>
+      <extrude>0</extrude>
+      <altitudeMode>relativeToGround</altitudeMode>
+      <outerBoundaryIs>
         <LinearRing>
           <coordinates>
-            31.2365,30.0455,200
-            31.2365,30.0455,200
-            31.2365,30.0455,200
-            31.2365,30.0455,200
-            31.2365,30.0455,200
+            31.233,30.042,0
+            31.239,30.042,0
+            31.236,30.045,300
+            31.233,30.042,0
           </coordinates>
         </LinearRing>
-      </innerBoundaryIs>
+      </outerBoundaryIs>
+    </Polygon>
+  </Placemark>
+
+  <!-- Face 2 (East) -->
+  <Placemark>
+    <name>Pyramid Face East</name>
+    <styleUrl>#pyramidFace2</styleUrl>
+    <Polygon>
+      <extrude>0</extrude>
+      <altitudeMode>relativeToGround</altitudeMode>
+      <outerBoundaryIs>
+        <LinearRing>
+          <coordinates>
+            31.239,30.042,0
+            31.239,30.048,0
+            31.236,30.045,300
+            31.239,30.042,0
+          </coordinates>
+        </LinearRing>
+      </outerBoundaryIs>
+    </Polygon>
+  </Placemark>
+
+  <!-- Face 3 (South) -->
+  <Placemark>
+    <name>Pyramid Face South</name>
+    <styleUrl>#pyramidFace3</styleUrl>
+    <Polygon>
+      <extrude>0</extrude>
+      <altitudeMode>relativeToGround</altitudeMode>
+      <outerBoundaryIs>
+        <LinearRing>
+          <coordinates>
+            31.239,30.048,0
+            31.233,30.048,0
+            31.236,30.045,300
+            31.239,30.048,0
+          </coordinates>
+        </LinearRing>
+      </outerBoundaryIs>
+    </Polygon>
+  </Placemark>
+
+  <!-- Face 4 (West) -->
+  <Placemark>
+    <name>Pyramid Face West</name>
+    <styleUrl>#pyramidFace4</styleUrl>
+    <Polygon>
+      <extrude>0</extrude>
+      <altitudeMode>relativeToGround</altitudeMode>
+      <outerBoundaryIs>
+        <LinearRing>
+          <coordinates>
+            31.233,30.048,0
+            31.233,30.042,0
+            31.236,30.045,300
+            31.233,30.048,0
+          </coordinates>
+        </LinearRing>
+      </outerBoundaryIs>
     </Polygon>
   </Placemark>
 </Document>
